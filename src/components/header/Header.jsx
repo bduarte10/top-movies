2fa17004655145c450015539064275c0257d3d07
Bruno <duartebruno2208@gmail.com>
2024-02-23 19:12:06 -0300

    fix: header padding

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import { SearchBar } from './SearchBar'
 
 export function Header() {
   return (
-    <header className="p-4 xl:p-0 bg-slate-700 w-full">
+    <header className="p-4 bg-slate-700 w-full">
       <div className="max-w-7xl h-full flex flex-col sm:flex-row items-center justify-between mx-auto">
         <Link to="/">
           <h1 className="font-bold text-2xl text-slate-100">TopMovies</h1>
